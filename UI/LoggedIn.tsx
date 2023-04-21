5edf22d05944c20a00a0903d8c3466b0c0254d67
Mikolaj Filipkowski <mikolajfilipkowski000@gmail.com>
2023-04-21 09:44:10 +0200

    Add icons

--- UI/LoggedIn.tsx
+++ UI/LoggedIn.tsx
@@ -5,6 +5,8 @@ import { auth } from '../firebase.js'
 import ChatScreen from '../screens/ChatScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import { Ionicons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +15,28 @@ export default function LoggedIn() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Chat" component={ChatScreen} />
-                <Tab.Screen name="Profile" component={ProfileScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen name="Chat" component={ChatScreen}  
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="home" size={size} color={color} />
+                        )
+                    }}/>
+                <Tab.Screen name="Profile" component={ProfileScreen}  
+                    options={{
+                        tabBarLabel: 'Profile',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="account-circle" size={size} color={color} />
+                        )
+                    }}/>
+                <Tab.Screen name="Settings" component={SettingsScreen} 
+                    options={{
+                        tabBarLabel: 'Settings',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="settings-sharp" size={size} color={color} />
+                        )
+                    }}
+                    />
             </Tab.Navigator>
         </NavigationContainer>
     )
